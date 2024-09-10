25271dc1dd5eb0e61926359cdce7e4ebe30c045d
Souvanxay LUANGSOUVANNAVONG <souvanxay38@gmail.com>
2024-09-10 15:37:18 +0200

    Add Preferences, LocalNotification and Auth tests

--- WebSharper.Capacitor.Sample/Client.fs
+++ WebSharper.Capacitor.Sample/Client.fs
@@ -25,8 +25,8 @@ module Client =
         return image
     }
 
-    let writeToClipboard () = promise {
-        let! clipboard = Capacitor.Clipboard.Write(Clipboard.WriteOptions(String = "Hello World!"))
+    let writeToClipboard value = promise {
+        let! clipboard = Capacitor.Clipboard.Write(Clipboard.WriteOptions(String = $"{value}"))
         return clipboard
     }
 
@@ -52,56 +52,144 @@ module Client =
             Title = "Confirm",
             Message = "Are you sure you'd like to press the red button?"
         ))
-
         return value
+    }
+
+    let save value = promise {
+        let! token = Capacitor.Preferences.Set(Preferences.SetOptions(
+            key = "auth_token",
+            value = value
+        ))
+        return token
+    }
+
+    let get() = promise {
+        let! token = Capacitor.Preferences.Get(Preferences.GetOptions(
+            key = "auth_token"
+        ))
+        return token
+    }
+
+    [<AutoOpen>]
+    module AuthTest = 
+        type User (name: string, password: string) = 
+            member x.Name = name
+            member x.Password = password
+
+        let saveToken token = promise {
+            Capacitor.Preferences.Set(Preferences.SetOptions(
+                key = "auth_token",
+                value = token
+            )) |> ignore
+        }
+
+        let getToken() = promise {
+            let! value = Capacitor.Preferences.Get(Preferences.GetOptions(
+                key = "auth_token"
+            ))
+            return value
+        }
+
+        let login() = promise {
+            let data = User(name = "Got", password = "password")
+            let! response = Capacitor.Http.Post(Http.HttpOptions(
+                url = "http://localhost:5173/",
+                Data = data
+            ))
+
+            if (response.Status = 200) then
+                let token = response?data?token
+                Capacitor.Dialog.Alert(Dialog.AlertOptions(Message = $"Logged in, token: {token}")) |> ignore
+                do! saveToken token  
+            else 
+                Capacitor.Dialog.Alert(Dialog.AlertOptions(Message = $"Login failed: {response}")) |> ignore
+        }
+
+    let scheduleNotification() = promise {
+        let notificationOptions = LocalNotifications.ScheduleOptions(
+            Notifications = [|LocalNotifications.LocalNotificationSchema(
+            Id = 1,
+            Title = "Reminder Notification",
+            Body = "Explore new variety and offers",
+            LargeBody = "Get 30% discounts on new products",
+            SummaryText = "Exciting offers"
+        )|]
+        )
+
+        let! notification = Capacitor.Capacitor.LocalNotifications.Schedule(notificationOptions) 
+        return notification
+    }
+
+    let getDeliveredNotification() = promise {
+        let! deliver = Capacitor.LocalNotifications.GetDeliveredNotifications() 
+        return deliver
     }
 
     [<SPAEntryPoint>]
     let Main () =
-        let newName = Var.Create ""
+        let preferencesValue = Var.Create ""
+        let clipboardValue = Var.Create ""
 
         IndexTemplate.Main()
-            .ListContainer(
-                People.View.DocSeqCached(fun (name: string) ->
-                    IndexTemplate.ListItem().Name(name).Doc()
-                )
-            )
-            .Name(newName)
-            .Add(fun _ ->
-                People.Add(newName.Value)
-                newName.Value <- ""
-            )
             .TakePhoto(fun _ ->
                 async {
-                    return! takePicture().Then(fun image -> Var.Set newName <| image.Base64String.Substring(0, 20)).AsAsync()
+                    return! takePicture().Then(fun image -> printfn $"Photo image: {image.Base64String.Substring(0, 20)}" ).AsAsync()
                 }
                 |> Async.Start
             )
+            .ClipboardValue(clipboardValue)
             .WriteClipboard(fun _ ->
                 async {
-                    printfn "Successfully write 'Hello World!' to clipboard"
-                    return! writeToClipboard().Then(fun _ -> Var.Set newName <| "Successfully write 'Hello World!' to clipboard").AsAsync()
+                    return! writeToClipboard(clipboardValue.Value).Then(fun _ -> printfn $"Successfully write '{clipboardValue.Value}' to clipboard").AsAsync()
                 }
                 |> Async.Start
             )
             .SetCookies(fun _ ->
                 async {
-                    printfn "Successfully Set Cookies"
-                    return! writeToClipboard().Then(fun _ -> Var.Set newName <| "Successfully Set Cookies").AsAsync()
+                    return! setCookies().Then(fun _ -> printfn "Successfully Set Cookies").AsAsync()
                 }
                 |> Async.Start
             )
             .ShowAlert(fun _ ->
                 async {
-                    printfn "Successfully Show Alert"
-                    return! showAlert().Then(fun alert -> Var.Set newName <| "Successfully Show Alert").AsAsync()
+                    return! showAlert().Then(fun alert -> printfn "Successfully Show Alert").AsAsync()
                 }
                 |> Async.Start
             )
             .ShowConfirm(fun _ ->
                 async {
-                    printfn "Successfully Show Confirm"
-                    return! showConfirm().Then(fun confirm -> Var.Set newName <| "Successfully Show Confirm").AsAsync()
+                    return! showConfirm().Then(fun confirm -> printfn "Successfully Show Confirm").AsAsync()
+                }
+                |> Async.Start
+            )
+            .PreferencesValue(preferencesValue)
+            .Save(fun _ -> 
+                async {
+                    return! save(preferencesValue.Value).Then(fun _ -> printfn "Save value Successfully").AsAsync()
+                }
+                |> Async.Start
+            )
+            .Get(fun _ -> 
+                async {
+                    return! get().Then(fun token -> printfn $"Value: {token.Value}").AsAsync()
+                }
+                |> Async.Start
+            )
+            .LogIn(fun _ -> 
+                async {
+                    return! login().Then(fun _ -> printfn "").AsAsync()
+                }
+                |> Async.Start
+            )
+            .Notification(fun _ -> 
+                async {
+                    return! scheduleNotification().Then(fun notification -> printfn $"Scheduled notification complete").AsAsync()
+                }
+                |> Async.Start
+            )
+            .DeliveredNotification(fun _ -> 
+                async {
+                    return! getDeliveredNotification().Then(fun notification -> JS.Alert(JSON.Stringify(notification))).AsAsync()
                 }
                 |> Async.Start
             )
